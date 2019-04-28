3884963f1d027d6702fdc2eb4f08434ee6b17294
jinaroo <sohnjin@gmail.com>
2019-04-28 03:39:54 -0400

    attempted to have player be able to rotate while they are moving forward

--- CodeLab1-3DGame/Assets/Scripts/PlayerController.cs
+++ CodeLab1-3DGame/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {    
     public float speed = 1f;
     public float orbitRadius = 5.5f;
+    public float turnSpeed = 1f;
     
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,14 @@ public class PlayerController : MonoBehaviour
     {   
         if (Input.GetKey(KeyCode.W))
         {
-            Vector3 targetPos = transform.position + transform.forward * speed * Time.deltaTime; // where we are now plus where we want to be
+            Vector3 forwardDir = transform.forward;
+
+            if (Input.GetKey(KeyCode.D))
+            {
+//                forwardDir = Quaternion.AngleAxis(turnSpeed * Time.deltaTime, transform.up) * forwardDir;
+            }
+            
+            Vector3 targetPos = transform.position + forwardDir * speed * Time.deltaTime; // where we are now plus where we want to be
            
             Vector3 targetMoveDir = targetPos - transform.position; // b - a
 
@@ -28,11 +36,20 @@ public class PlayerController : MonoBehaviour
         
             transform.forward = Vector3.Cross(newUp, newRight).normalized; // movement direction tangent to sphere
 
-            transform.position += transform.forward * speed * Time.deltaTime; // move in the correct direction
+            transform.position += forwardDir * speed * Time.deltaTime; // move in the correct direction
 
             transform.position = transform.position.normalized * orbitRadius; // stops him from flying away by sticking to orbitRadius
 
             transform.up = newUp.normalized; // keeps him oriented upwards
-        }   
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            // none of the below rotates the player properly
+//            transform.RotateAround(transform.up, turnSpeed * Time.deltaTime);
+//            transform.RotateAround(transform.position, transform.up, turnSpeed * Time.deltaTime);
+//            transform.Rotate(transform.up, turnSpeed * Time.deltaTime, Space.Self);
+//            transform.forward = Quaternion.AngleAxis(turnSpeed * Time.deltaTime, Vector3.up) * transform.forward; // rotate player around transform.up
+        }
     }
 }
