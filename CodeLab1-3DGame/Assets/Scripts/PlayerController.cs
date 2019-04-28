67955f9c4c13f0ef51fc0e7deda8d7148ba59cf4
jinaroo <sohnjin@gmail.com>
2019-04-28 03:12:54 -0400

    updated PlayerController so that player walks tangent to sphere

--- CodeLab1-3DGame/Assets/Scripts/PlayerController.cs
+++ CodeLab1-3DGame/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{    
+    public float speed = 1f;
+    public float orbitRadius = 5.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +15,24 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 targetPos = transform.position + transform.forward * speed * Time.deltaTime; // where we are now plus where we want to be
+           
+            Vector3 targetMoveDir = targetPos - transform.position; // b - a
+
+            Vector3 newUp = transform.position.normalized * orbitRadius; // fixed length vector that points out from the sphere
+            
+            Vector3 newRight = Vector3.Cross(targetMoveDir, newUp).normalized; // always points towards the right
         
+            transform.forward = Vector3.Cross(newUp, newRight).normalized; // movement direction tangent to sphere
+
+            transform.position += transform.forward * speed * Time.deltaTime; // move in the correct direction
+
+            transform.position = transform.position.normalized * orbitRadius; // stops him from flying away by sticking to orbitRadius
+
+            transform.up = newUp.normalized; // keeps him oriented upwards
+        }   
     }
 }
